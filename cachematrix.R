8a42dee9a108202e74bd7e2fe84fce36cba1f53b
hourajg <146432580+hourajg@users.noreply.github.com>
2023-09-29 15:24:13 +0330

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,1 +1,30 @@
-
+# The makeCacheMatrix() function is a custom function that allows you to create a specialized matrix object. 
+# This object has additional features that enable caching and retrieval of data. 
+# Typically, this function is utilized when performing matrix calculations or computations that require repetitive calculations on the same matrix. 
+# The main purpose of using makeCacheMatrix() is to enhance performance by storing intermediate results in a cache. 
+# For instance, if you have a complex operation that involves calculating the inverse of a matrix multiple times, you can employ this function to save the inverse in the cache after it has been computed once. 
+# As a result, subsequent requests for the inverse will avoid unnecessary calculations, leading to improved efficiency.
+> makeCacheMatrix <- function(x=matrix()){
++ n1 <-null
++ set <-function(m1){
++ x<<-m1
++ n1 <<-NULL
++ }
++ get <-function()x
++ setinverse <-function(solve) n1 <<-solve
++ getinverse <-function ()n1
++ list (set =set, get=get, serinverse =setinverse, getinverse=getinverse)
++ }
+# "cacheSolve" is a function in RStudio that is used to calculate the inverse of a matrix and cache the result for future use. 
+# It is particularly useful when working with large matrices, as the inverse calculation can be computationally expensive and time-consuming. 
+# By caching the result, subsequent calls to "cacheSolve" with the same matrix can retrieve the pre-calculated inverse, saving computational time.
+> cacheSolve <-function(x,...){
++ n1 <-x$getinverse()
++ if(!is.null(n1)){
++ message("getting cached data")
++ return(n1)}
++ data <-x$get()
++ n1 <-solve(data)
++ x$setinverse(n1)
++ n1
++ }
